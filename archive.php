3082c7683aaa33bc4be6d313e531b1bceb6428c4
darcelvictor <darcel.v@gmail.com>
2019-11-14 19:30:30 +0100

    add publication, fix particle space desktop

---
+++ archive.php
@@ -0,0 +1,13 @@
+<?php get_header();
+/*
+Template Name: archive
+*/
+?> 
+   <section id="contentPage">
+	   <?php include 'template-content-archive.php';?>
+	</section>
+    <section id="teamContent">
+   <div>
+    </div>
+    </section>
+<?php get_footer();?>
