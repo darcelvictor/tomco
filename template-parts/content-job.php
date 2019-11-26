0e4995a8f0b88fd44355fe45ec4248a7172a6279
darcelvictor <darcel.v@gmail.com>
2019-11-26 19:02:41 +0100

    fix carries

--- template-parts/content-job.php
+++ template-parts/content-job.php
@@ -1,8 +1,6 @@
-<section id="job">
+<section id="singleJobContent">
 <h1><?php the_title(); ?></h1>
 <?php the_content(); ?>
-</section>
-<section>
 <div id="contactBttn">
     <a class="Bttn grBttn" href="/contact">contactez nous <i class="fas fa-rocket"></i></a>
 </div>
