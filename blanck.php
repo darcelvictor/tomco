1c29da23b6664024f4a3b59765ab4769bdaa8e8c
Victor Darcel <darcel.v@gmail.com>
2020-03-06 15:37:15 +0100

    add conf politics and blanck page

---
+++ blanck.php
@@ -0,0 +1,20 @@
+<?php get_header();
+/*
+Template Name: blanck
+*/
+?> 
+   <section id="contentPage">
+   <h1><?php the_title();?></h1>
+   <?php
+    // TO SHOW THE PAGE CONTENTS
+    while ( have_posts() ) : the_post(); ?> <!--Because the_content() works only inside a WP Loop -->
+        <div class="entry-content-page">
+            <?php the_content(); ?> <!-- Page Content -->
+        </div><!-- .entry-content-page -->
+
+    <?php
+    endwhile; //resetting the page loop
+    wp_reset_query(); //resetting the page query
+    ?>
+    </section>
+<?php get_footer();?>
